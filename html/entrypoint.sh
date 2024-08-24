49951a267c9599e69d00d771e1c828f94fb928ba
Trainersudhanshu <124223047+Trainersudhanshu@users.noreply.github.com>
2024-08-24 14:30:07 +0530

    Update entrypoint.sh

--- html/entrypoint.sh
+++ html/entrypoint.sh
@@ -4,5 +4,12 @@
 envsubst '${API_URL}' < /usr/share/nginx/html/gallery.html > /usr/share/nginx/html/gallery.tmp.html
 mv /usr/share/nginx/html/gallery.tmp.html /usr/share/nginx/html/gallery.html
 
+envsubst '${API_URL}' < /usr/share/nginx/html/register.html > /usr/share/nginx/html/register.tmp.html
+mv /usr/share/nginx/html/register.tmp.html /usr/share/nginx/html/register.html
+
+envsubst '${API_URL}' < /usr/share/nginx/html/watch.html > /usr/share/nginx/html/watch.tmp.html
+mv /usr/share/nginx/html/watch.tmp.html /usr/share/nginx/html/watch.html
+
+
 # Start Nginx
 nginx -g 'daemon off;'
